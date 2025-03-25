f86815ce4687b554fc601084048c1bf973b1e7e3
dragonfly <sudarshanjaisankarxc@gmail.com>
2025-03-25 23:17:45 +0530

    Removed Bugs

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ pygame.display.set_caption("Hangman")
 
 # boolean variables
 running = True
+mainmenu_in = False
 main_menu_fade_out = False
 game_play_fade_in = False
 game_play_fade_out = False
+game_over = False
 
 
 # temp vars
@@ -25,7 +27,8 @@ component = ["head", "body", "arm-left", "arm-right", "leg-left", "leg-right"]
 
 while running:
     word = "MISSISIPPI"
-    mainmenu_window = mainmenu(window, (660, ypos)) 
+    mainmenu_window = mainmenu(window, (660, ypos))
+    gameover_window = gameover(window, word, (660, ypos_game_over))
     main_window = mainwindow(window, (300, ypos_gameplay), word, input_string, man_order, component)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -34,8 +37,25 @@ while running:
             if mainmenu_window.key_input(event.key):
                 main_menu_fade_out = True
                 game_play_fade_in = True
-            if game_play_fade_in:
+            elif game_play_fade_in:
                 letter = main_window.get_input(event.key)
+            if game_over:
+                print("Got here")
+                if gameover_window.key_input(event.key): 
+                        mainmenu_in = True
+                        game_over = False 
+                        game_play_fade_in = True
+                        game_play_fade_out = False
+                        ypos_gameplay = 2000
+                        component = ["head", "body", "arm-left", "arm-right", "leg-left", "leg-right"]
+                        man_order = []
+
+
+
+    if mainmenu_in == True:
+        ypos_game_over = 1000
+        if ypos_game_over != -1000:
+            ypos_game_over -= 10
 
     if main_menu_fade_out:
         if ypos != -500:
@@ -60,18 +80,24 @@ while running:
             man_order = []
             component = ["head", "body", "arm-left", "arm-right", "leg-left", "leg-right"]
 
-    if not component:
+    print(ypos_game_over)
+
+    if len(man_order) == 6:
+        mainmenu_in = False
         if ypos_gameplay != -500:
             ypos_gameplay -= 10
             if ypos_game_over != 330:
+                print("executing")
                 ypos_game_over -= 10
+            else:
+                game_over = True
 
     main_window.draw_input()
     main_window.draw_text()
     main_window.render_gallows()
     main_window.draw_man()
+    gameover_window.build()
     mainmenu_window.build()
-    gameover(window, word, (660, ypos_game_over)).build()
 
     pygame.display.flip()
 
